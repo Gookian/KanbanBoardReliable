b24e9db018e5783721bfc22ee5b8c1e1f60c1fdb
Daniil <48077576+Gookian@users.noreply.github.com>
2022-04-22 09:56:49 +0700

    Добавил ServerAPI к десктопному клиенту

---
+++ Kanban.DesktopClient/ConnectionManager.cs
@@ -0,0 +1,34 @@
+﻿using Core;
+
+namespace Kanban.DesktopClient
+{
+    using System;
+    using System.Net.WebSockets;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Kanban.DesktopClient.RestAPI;
+
+    class ConnectionManager
+    {
+        private readonly string _api;
+        private ClientWebSocket client;
+        private MessageService messageService;
+
+        public ConnectionManager(string api)
+        {
+            _api = api;
+        }
+
+        public async void StartConnection()
+        {
+            client = new ClientWebSocket();
+            await client.ConnectAsync(new Uri($"ws://localhost:5000/{_api}"), CancellationToken.None);
+            messageService = new MessageService(client);
+            ServerAPI.MessageService = messageService;
+
+            Console.WriteLine("Connected to server");
+
+            messageService.ResponseAsync();
+        }
+    }
+}
